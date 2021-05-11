13e756b1ec3d49b3e2c2aac71c7fbd84784c10df
Thomas Li <guslithd@student.gu.se>
2021-05-11 21:48:39 +0200

    Example: remove garbage from Simple.hs

--- src/Haski/Example/Simple.hs
+++ src/Haski/Example/Simple.hs
@@ -1,14 +1,10 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecursiveDo #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TemplateHaskell #-}
 
 module Haski.Example.Simple where
 
-import Data.Int
 import Haski.Lang
-import Haski.TH
-import Text.PrettyPrint.HughesPJClass (Pretty,pPrint,text)
+
 
 -- [0, 1, 2, 3,...]
 nats :: Haski (Stream Int)
@@ -57,44 +53,3 @@ countingCall = do
     let tick = val True
     top <- alt
     counting tick top
-
-
--- Testing simple CaseOf-style pattern matching stuff
-
-alter :: Stream Int -> Haski (Stream Int)
-alter s = s `caseof` \case
-    T1 n b -> n + 1
-    T2     -> s
-
-type HELLO = Stream Int
-
-data T = T1 HELLO (Stream Bool) | T2
-$(mkConstructors ''T)
-instance Partition Int T where
-    partition =
-        [ \ v -> (v `gtE` 3, _T1 (v * 999) (v `gtE` 100))
-        , \ v -> (val True, _T2)
-        ]
-
-data WrapInt = WrapInt (Stream Int)
-$(mkConstructors ''WrapInt)
-instance Partition Bool WrapInt where
-    partition =
-        [ \ v -> (v, _WrapInt 100)
-        , \ v -> (ifte v (val False) (val True), _WrapInt 99)
-        ]
-
-pm1 :: Stream Int -> Stream Bool -> Haski (Stream Int)
-pm1 = node "pmnode" $ \ ns bs -> mdo
-    x <- ns `caseof` \case
-        T1 n b -> b
-        T2     -> val False
-    y <- x `caseof` \case
-        WrapInt n -> n
-    return y
-
-pm1call :: Haski (Stream Int)
-pm1call = do
-    ns <- nats
-    bs <- alt
-    pm1 ns bs
